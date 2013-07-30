003c5ce70ac7b300186cb65105e7212cf72e045a
Aditya Kamath <cs11b001@iith.ac.in>
2013-07-30 15:15:37 +0530

    first assignment final

--- lib/libc/sys-minix/execve.c
+++ lib/libc/sys-minix/execve.c
@@ -114,6 +114,7 @@ int execve(const char *path, char * const *argv, char * const *envp)
 	/* We can finally make the system call. */
 	m.m1_i1 = strlen(path) + 1;
 	m.m1_i2 = frame_size;
+	fprintf(stderr, " execve %s\n", path);
 	m.m1_p1 = (char *) __UNCONST(path);
 	m.m1_p2 = frame;
 
